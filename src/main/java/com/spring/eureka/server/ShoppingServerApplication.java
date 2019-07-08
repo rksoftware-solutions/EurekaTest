33b58580af73abf4b8a6dd9653f9ec9a53015ffd
SINHA JEE <SINHA JEE@LAPTOP-K4U8G5DV>
2019-07-08 19:04:51 +0530

    Changes done in ShoppingServerApplication class.

--- src/main/java/com/spring/eureka/server/ShoppingServerApplication.java
+++ src/main/java/com/spring/eureka/server/ShoppingServerApplication.java
@@ -16,6 +16,18 @@ public class ShoppingServerApplication {
 
 	public static void main(String[] args) {
 		
+		/*
+		 * RestTemplate rest = new RestTemplate(); HttpHeaders headers = new
+		 * HttpHeaders(); String body = "";
+		 * 
+		 * HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+		 * ResponseEntity<String> responseEntity = rest.exchange(
+		 * "https://anypoint.mulesoft.com/mocking/api/v1/links/d6c96159-a38a-42cc-96fe-3f9f1116f97e/songs",
+		 * HttpMethod.GET, requestEntity, String.class); HttpStatus status =
+		 * responseEntity.getStatusCode(); String response = responseEntity.getBody();
+		 * System.out.println("Response status: " + status);
+		 * System.out.println(response);
+		 */
 		SpringApplication.run(ShoppingServerApplication.class, args);
 	}
 
